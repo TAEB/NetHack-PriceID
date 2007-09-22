191f4708c5c34baf9c1f0fa8de1d9848908e1274
Shawn M Moore <sartak@gmail.com>
2007-09-22 14:11:27 -0500

    Add new functions to export_ok

--- lib/NetHack/PriceID.pm
+++ lib/NetHack/PriceID.pm
@@ -3,7 +3,7 @@ package NetHack::PriceID;
 use strict;
 use warnings;
 use parent 'Exporter';
-our @EXPORT_OK = qw(priceid);
+our @EXPORT_OK = qw(priceid priceid_buy priceid_sell priceid_base);
 
 our %glyph2type =
 (
