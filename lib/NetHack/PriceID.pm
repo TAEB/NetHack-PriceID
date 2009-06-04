5113b5701093d40dd26314d70f1b1a6fa90420ba
Shawn M Moore <sartak@gmail.com>
2009-06-04 15:05:04 -0400

    Copyright year

--- lib/NetHack/PriceID.pm
+++ lib/NetHack/PriceID.pm
@@ -641,7 +641,7 @@ L<http://search.cpan.org/dist/NetHack-PriceID>
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright 2007 Shawn M Moore.
+Copyright 2007-2009 Shawn M Moore.
 
 This program is free software; you can redistribute it and/or modify it
 under the same terms as Perl itself.
